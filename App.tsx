4c372ac98379d3b0dd10da18b64f596f10967792
tm7218921 <tm7218921@gmail.com>
2026-01-18 21:07:36 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
   const [data, setData] = useState<PortfolioData>(INITIAL_PORTFOLIO);
 
   const handleDownloadResume = () => {
-    window.open('/Tanmay-Mandal-Resume.pdf', '_blank');
+    window.open('/Tanmay_Mandal_Resume.pdf', '_blank');
   };
 
   return (
